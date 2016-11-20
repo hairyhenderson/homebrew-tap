cddae2d6dc58d89bdf4b74c8dc685221b446167b
Dave Henderson <dhenderson@gmail.com>
2016-11-19 19:08:18 -0500

    Released gomplate 1.2.2
    Signed-off-by: Dave Henderson <dhenderson@gmail.com>

--- gomplate.rb
+++ gomplate.rb
@@ -1,9 +1,9 @@
 class Gomplate < Formula
   desc "A commandline Golang template processor"
   homepage "https://github.com/hairyhenderson/gomplate"
-  url "https://github.com/hairyhenderson/gomplate/releases/download/v1.2.1/gomplate_darwin-amd64"
-  sha256 "608dfd31a6eec3151a837caf58701011c698d2ca5d4b3fa85a12b4fb55eabc4f"
-  version "1.2.1"
+  url "https://github.com/hairyhenderson/gomplate/releases/download/v1.2.2/gomplate_darwin-amd64"
+  sha256 "67f10cc4a88d1c7ca260f944fb015b68fbb383089d7da84a3e9e26075a250f7a"
+  version "1.2.2"
 
   depends_on :arch => :x86_64
 
