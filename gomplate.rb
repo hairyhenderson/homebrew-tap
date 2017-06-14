c303b8145d937e4dc5e2491ca7a7a8532c9be7df
Dave Henderson <dhenderson@gmail.com>
2017-06-14 01:01:36 -0400

    Update gomplate to 1.9.0
    Signed-off-by: Dave Henderson <dhenderson@gmail.com>

--- gomplate.rb
+++ gomplate.rb
@@ -1,9 +1,9 @@
 class Gomplate < Formula
   desc "A commandline Golang template processor"
   homepage "https://github.com/hairyhenderson/gomplate"
-  url "https://github.com/hairyhenderson/gomplate/releases/download/v1.8.0/gomplate_darwin-amd64"
-  sha256 "5fd9d1c76d8babc9e076d50b31e212e5eee0d22d63ab36abe7066256b7c0fac6"
-  version "1.8.0"
+  url "https://github.com/hairyhenderson/gomplate/releases/download/v1.9.0/gomplate_darwin-amd64"
+  sha256 "8a672e4af2190e902a451f77808d533d86f466edc50392887b7ca51f61e2be6d"
+  version "1.9.0"
 
   depends_on :arch => :x86_64
 
