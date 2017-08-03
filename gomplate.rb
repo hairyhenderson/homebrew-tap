3e1e0602a2ce0e9dbaf17dc23b9c79a7933019f7
Dave Henderson <dhenderson@gmail.com>
2017-08-03 16:26:23 -0400

    Upgrading gomplate to 1.10.0
    Signed-off-by: Dave Henderson <dhenderson@gmail.com>

--- gomplate.rb
+++ gomplate.rb
@@ -1,9 +1,9 @@
 class Gomplate < Formula
   desc "A commandline Golang template processor"
   homepage "https://github.com/hairyhenderson/gomplate"
-  url "https://github.com/hairyhenderson/gomplate/releases/download/v1.9.1/gomplate_darwin-amd64"
-  sha256 "f55c3743d770d3cfbbb12cc8b3c166011f716a39d32856a0d87faa510e4db3b0"
-  version "1.9.1"
+  url "https://github.com/hairyhenderson/gomplate/releases/download/v1.10.1/gomplate_darwin-amd64"
+  sha256 "d154aa62f04a79c8da28e1af9350aeee49d6527930bcf9823dda691ea21b5f6b"
+  version "1.10.0"
 
   depends_on :arch => :x86_64
 
