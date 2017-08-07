4f9f67d19e971a229aeb5677243e042435c10be0
Mohsen <mohsen0@users.noreply.github.com>
2017-08-07 14:07:35 +0100

    Fixed the wrong version, there is no 1.10.1 yet

--- gomplate.rb
+++ gomplate.rb
@@ -1,7 +1,7 @@
 class Gomplate < Formula
   desc "A commandline Golang template processor"
   homepage "https://github.com/hairyhenderson/gomplate"
-  url "https://github.com/hairyhenderson/gomplate/releases/download/v1.10.1/gomplate_darwin-amd64"
+  url "https://github.com/hairyhenderson/gomplate/releases/download/v1.10.0/gomplate_darwin-amd64"
   sha256 "d154aa62f04a79c8da28e1af9350aeee49d6527930bcf9823dda691ea21b5f6b"
   version "1.10.0"
 
