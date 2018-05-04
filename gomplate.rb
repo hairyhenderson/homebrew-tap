7b5d0eb2b5d0ff90f9ac43bc4649a079e86ebb97
Dave Henderson <dhenderson@gmail.com>
2018-05-04 00:27:37 -0400

    Upgrading gomplate to v2.5.0
    Signed-off-by: Dave Henderson <dhenderson@gmail.com>

--- gomplate.rb
+++ gomplate.rb
@@ -1,9 +1,9 @@
 class Gomplate < Formula
   desc "A commandline Golang template processor"
   homepage "https://github.com/hairyhenderson/gomplate"
-  url "https://github.com/hairyhenderson/gomplate/releases/download/v2.4.0/gomplate_darwin-amd64-slim"
-  sha256 "cf14f5b9cbf1815a6c2878ea69b18d83c8e35904f11aa02a29d67a52ad75d666"
-  version "2.4.0"
+  url "https://github.com/hairyhenderson/gomplate/releases/download/v2.5.0/gomplate_darwin-amd64-slim"
+  sha256 "2506f284c785cfd67392c1f61c57929e034a8eeee6f18ef621814d042a0598f3"
+  version "2.5.0"
 
   depends_on :arch => :x86_64
 
