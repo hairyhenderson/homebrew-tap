7f5eb368250e68abb8e99b63d166f48c68653273
Dave Henderson <dhenderson@gmail.com>
2018-08-14 10:19:42 -0400

    Fixing sha256sum for binary
    Signed-off-by: Dave Henderson <dhenderson@gmail.com>

--- gomplate.rb
+++ gomplate.rb
@@ -2,7 +2,7 @@ class Gomplate < Formula
   desc "A commandline Golang template processor"
   homepage "https://github.com/hairyhenderson/gomplate"
   url "https://github.com/hairyhenderson/gomplate/releases/download/v2.7.0/gomplate_darwin-amd64-slim"
-  sha256 "c9be7f326cba2232bddec9c37ca77f111539e8157fc2cc0907c5815cb86e5dd9"
+  sha256 "0d572c033dbfa0af8133b738d9f6487b3eb361d7933590c6d3c1c5b6f8df6b6d"
   version "2.7.0"
 
   depends_on :arch => :x86_64
