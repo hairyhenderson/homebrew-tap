1cba45e02a413cf6594602d43a48f02d821834b4
Dave Henderson <dhenderson@gmail.com>
2018-09-25 00:14:54 -0400

    Fixing the hash yet again...
    Signed-off-by: Dave Henderson <dhenderson@gmail.com>

--- gomplate.rb
+++ gomplate.rb
@@ -2,7 +2,7 @@ class Gomplate < Formula
   desc "A commandline Golang template processor"
   homepage "https://github.com/hairyhenderson/gomplate"
   url "https://github.com/hairyhenderson/gomplate/releases/download/v3.0.0/gomplate_darwin-amd64-slim"
-  sha256 "4d9473681995c8fa87338edb2ddcb136f58abc72565d1ea2ba1b7dc3258c420e"
+  sha256 "10b73c2a1f1c4a69591bc70b37adc4e361ad27bb0339b29999370e08ac114beb"
   version "3.0.0"
 
   depends_on :arch => :x86_64
