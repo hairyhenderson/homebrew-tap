0889d6cf1512677b889d04448122f4b2235b8c6f
Dave Henderson <dhenderson@gmail.com>
2018-11-30 10:30:32 -0500

    Updating checksum for properly-compressed binary
    Signed-off-by: Dave Henderson <dhenderson@gmail.com>

--- gomplate.rb
+++ gomplate.rb
@@ -2,7 +2,7 @@ class Gomplate < Formula
   desc "A commandline Golang template processor"
   homepage "https://github.com/hairyhenderson/gomplate"
   url "https://github.com/hairyhenderson/gomplate/releases/download/v3.1.0/gomplate_darwin-amd64-slim"
-  sha256 "4ee3df466a050bcf019009448c122900e0b3aeebf2a860d9199e05ced3cf3921"
+  sha256 "79c86f163dd1d175dc9b02952e895dc5e0f3fac6d1c24753f4a113051bfd013e"
   version "3.1.0"
 
   depends_on :arch => :x86_64
