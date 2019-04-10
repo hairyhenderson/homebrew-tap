f6a2529ba1628c10c5fcee67f3d6a28726b38d0d
Dave Henderson <dhenderson@gmail.com>
2019-04-09 21:16:10 -0400

    New website
    Signed-off-by: Dave Henderson <dhenderson@gmail.com>

--- gomplate.rb
+++ gomplate.rb
@@ -1,6 +1,6 @@
 class Gomplate < Formula
-  desc "A commandline Golang template processor"
-  homepage "https://github.com/hairyhenderson/gomplate"
+  desc "A versatile Go template processor"
+  homepage "https://gomplate.ca"
   url "https://github.com/hairyhenderson/gomplate/releases/download/v3.3.1/gomplate_darwin-amd64-slim"
   sha256 "77dc4d844114b7b93e53a0cfee1223f9131050267f32f104815c2b5b6b85ff3c"
   version "3.3.1"
