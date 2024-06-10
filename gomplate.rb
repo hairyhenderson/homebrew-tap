3979afb6e30564975f872b036f17d960c71bfe1b
Dave Henderson <dhenderson@gmail.com>
2024-06-09 21:00:13 -0400

    Upgrading gomplate to 4.0.0-pre-3
    Signed-off-by: Dave Henderson <dhenderson@gmail.com>

--- gomplate.rb
+++ gomplate.rb
@@ -1,9 +1,9 @@
 class Gomplate < Formula
   desc "A versatile Go template processor"
   homepage "https://gomplate.ca"
-  url "https://github.com/hairyhenderson/gomplate/releases/download/v3.11.8/gomplate_darwin-amd64"
-  sha256 "c16574f309311f2ee5edd136634e5ab6a78c218f30c5f698f76038e173a7747d"
-  version "3.11.8"
+  url "https://github.com/hairyhenderson/gomplate/releases/download/v4.0.0-pre-3/gomplate_darwin-amd64"
+  sha256 "fb9668463299e2ecce3c229b08680bf6bb39be950dae5b5eefb24c76dd9db89b"
+  version "4.0.0-pre-3"
 
   depends_on :arch => :x86_64
 
