2e341d04ff3e5030e678f6d1e0f48c2f2ce30b4c
Dave Henderson <dhenderson@gmail.com>
2024-07-06 13:50:01 -0400

    fix broken sha
    Signed-off-by: Dave Henderson <dhenderson@gmail.com>

--- gomplate.rb
+++ gomplate.rb
@@ -2,7 +2,7 @@ class Gomplate < Formula
   desc "A versatile Go template processor"
   homepage "https://gomplate.ca"
   url "https://github.com/hairyhenderson/gomplate/releases/download/v4.1.0/gomplate_darwin-amd64"
-  sha256 ""
+  sha256 "7a8ae83654195d5ad516bd4bbe44bb671de8c7a89b440573adc648daf475ea64"
   version "4.1.0"
 
   depends_on :arch => :x86_64
